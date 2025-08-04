fb4e94ce228b32f527f0262b2179d05a81d0ca17
GreenieGuest <papootreasure@yahoo.com>
2025-08-04 04:08:22 -0500

    fix: https

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const textbox = document.getElementById("input");
 const div = document.getElementById("output");
+const apiKey = "38453222bd8526be0f30d941903e739f"
 
 const bracket_seed_order = [
     1, 64, 32, 33, 17, 48, 16, 49,
@@ -12,7 +13,18 @@ const bracket_seed_order = [
     15, 50, 18, 47, 31, 34, 2, 63
 ]
 
-async function getTopArtists(username, apiKey) {
+const colors = [
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red",
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red",
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red",
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red",
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red",
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red",
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red",
+    "crimson", "crimson", "red", "red", "crimson", "crimson", "red", "red" // this looks redundant but ill probably add more variety later
+]
+
+async function getTopArtists(username) {
     const url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username}&api_key=${apiKey}&format=json&limit=64`;
     try {
         const response = await fetch(url);
@@ -23,7 +35,13 @@ async function getTopArtists(username, apiKey) {
 			clearText()
 			for (var i = 0; i < 64; i++) {
 				let seed = bracket_seed_order[i];
-				displayText(`${seed}: ${artists[seed - 1].name}`);
+				let topTrack = await getTopArtistSong(artists[seed - 1].name);
+				console.log(topTrack);
+				displaySeed(seed, `${artists[seed - 1].name} - ${topTrack}`, colors[i]);
+				if ((i + 1) % 2 == 0) {
+					const hr = document.createElement("hr");
+					div.appendChild(hr);
+				}
 			}
 		} else {
 			clearText()
@@ -42,6 +60,32 @@ async function getTopArtists(username, apiKey) {
     }
 }
 
+async function getTopArtistSong(artistName) {
+    const url = `https://ws.audioscrobbler.com/2.0/?method=artist.getTopTracks&artist=${artistName}&api_key=${apiKey}&format=json&limit=1`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+		const tracks = data.toptracks.track;
+		
+		var trackName;
+		
+		if (tracks) {
+			trackName = tracks[0].name;
+		}
+        //console.log(data.topartists.artist); // Accessing track data
+		
+		//clearText()
+		//data.topartists.artist.forEach((artist, index) => {
+		//	displayText(`${index + 1}: ${artist.name}`);
+		//});
+		return trackName
+    } catch (error) {
+        console.error("Error fetching data:", error);
+		clearText();
+		displayText("Invalid profile");
+    }
+}
+
 function clearText() {
 	div.innerHTML = "";
 }
@@ -53,9 +97,16 @@ function displayText(str) {
 	div.appendChild(p);
 }
 
+function displaySeed(seed, str, color) {
+	const p = document.createElement("p");
+	p.innerHTML = `<span style="color: white; font-weight: bold; font-style: normal;">${seed} //</span> <span style="color: ${color};">${str}</span>`;
+	
+	div.appendChild(p);
+}
+
 function loadItUp() {
 	username = textbox.value;
-	getTopArtists(username, "38453222bd8526be0f30d941903e739f"); 
+	getTopArtists(username); 
 }
 
 document.getElementById("proceed").addEventListener("click", loadItUp);
